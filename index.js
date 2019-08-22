9d500278f76bc86851df7cb65bae6cac64a87c53
Samuele Dassatti <samueledassatti99@gmail.com>
2019-08-22 16:14:10 +0200

    Fragment matcher and error handler

--- index.js
+++ index.js
@@ -1,49 +1,76 @@
-const {HttpLink} = require("apollo-link-http")
-const {ApolloClient} = require("apollo-client")
-const {InMemoryCache} = require("apollo-cache-inmemory")
-const {WebSocketLink} = require("apollo-link-ws")
-const {split} = require("apollo-link")
-const {getMainDefinition} = require("apollo-utilities")
-const {SubscriptionClient} = require("subscriptions-transport-ws")
-const ws = require("ws")
-const fetch = require("node-fetch")
+const { HttpLink } = require('apollo-link-http');
+const { ApolloClient } = require('apollo-client');
+const {
+  InMemoryCache,
+  IntrospectionFragmentMatcher
+} = require('apollo-cache-inmemory');
+const { WebSocketLink } = require('apollo-link-ws');
+const { split } = require('apollo-link');
+const { getMainDefinition } = require('apollo-utilities');
+const { SubscriptionClient } = require('subscriptions-transport-ws');
+const ws = require('ws');
+const fetch = require('node-fetch');
 
-const createClient = (httpOptions, subscriptionOptions) => {
-    let link
-    const httpLink = new HttpLink({...httpOptions, fetch})
+const createClient = (
+  httpOptions,
+  subscriptionOptions,
+  errorHandler,
+  introspectionQueryResultData
+) => {
+  let link;
+  const httpLink = new HttpLink({ ...httpOptions, fetch });
 
-    if (subscriptionOptions) {
-        const subClient = new SubscriptionClient(
-            subscriptionOptions.uri,
-            subscriptionOptions.options,
-            ws
-        )
-        const wsLink = new WebSocketLink(subClient)
+  if (subscriptionOptions) {
+    const subClient = new SubscriptionClient(
+      subscriptionOptions.uri,
+      subscriptionOptions.options,
+      ws
+    );
+    const wsLink = new WebSocketLink(subClient);
 
-        link = split(
-            // split based on operation type
-            ({query}) => {
-                const {kind, operation} = getMainDefinition(query)
-                return (
-                    kind === "OperationDefinition" &&
-                    operation === "subscription"
-                )
-            },
-            wsLink,
-            httpLink
-        )
+    if (errorHandler) {
+      const errorLink = onError(errorHandler);
+
+      link = ApolloLink.from([errorLink, wsLink, httpLink]);
     } else {
-        link = httpLink
+      link = split(
+        // split based on operation type
+        ({ query }) => {
+          const { kind, operation } = getMainDefinition(query);
+          return kind === 'OperationDefinition' && operation === 'subscription';
+        },
+        wsLink,
+        httpLink
+      );
     }
+  } else {
+    if (errorHandler) {
+      const errorLink = onError(errorHandler);
 
-    const client = new ApolloClient({
-        // By default, this client will send queries to the
-        //  `/graphql` endpoint on the same host
-        link,
-        cache: new InMemoryCache(),
-    })
+      link = ApolloLink.from([errorLink, httpLink]);
+    } else {
+      link = httpLink;
+    }
+  }
 
-    return client
-}
+  let fragmentMatcher;
+  if (introspectionQueryResultData)
+    fragmentMatcher = new IntrospectionFragmentMatcher(
+      introspectionQueryResultData
+        ? {
+            introspectionQueryResultData
+          }
+        : {}
+    );
 
-module.exports = createClient
+  const client = new ApolloClient({
+    // By default, this client will send queries to the
+    //  `/graphql` endpoint on the same host
+    link,
+    cache: new InMemoryCache(introspectionQueryResultData?{ fragmentMatcher }:null)
+  });
+
+  return client;
+};
+
+module.exports = createClient;
